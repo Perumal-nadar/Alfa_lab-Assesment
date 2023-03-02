60247abc3e2c1aa3dc21a7bb874e2da8968752b0
Perumal-nadar <116861303+Perumal-nadar@users.noreply.github.com>
2023-03-02 11:15:04 +0530

    Update Q_no_06.java

--- Q_no_06.java
+++ Q_no_06.java
@@ -2,4 +2,23 @@ package alfaLab_Assesment;
 
 public class Q_no_06 {
 
+  public static void main(String[] args) {
+			
+			int i, j ,k;
+			for(i=1;i<=4;i++) {
+				for(j=4;j>=i;j--) {
+					System.out.print(" ");//print space
+				} 
+				for(j=1;j<=i;j++) {
+					System.out.print("*");//print star
+				}
+				for(k=2;k<=i;k++) {
+					System.out.print("*");//print star
+				}
+				System.out.println();
+			}
+			
+		
+		}
+  
 }
